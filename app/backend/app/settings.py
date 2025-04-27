e7e3174013edfd5f647482ed18bba6a0335fa56f
Ozgur Savascioglu <savasciogluozgur@gmail.com>
2025-04-27 22:23:29 +0300

    docker-compose file created

--- app/backend/app/settings.py
+++ app/backend/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'cooking_app'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '3346'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # <== important!
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
