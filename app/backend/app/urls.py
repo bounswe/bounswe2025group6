ef7ea588d618716f4fd51c622d2f26f329a7fec8
Ozgur Savascioglu <savasciogluozgur@gmail.com>
2025-04-28 00:37:07 +0300

    urls.py corrected

--- app/backend/app/urls.py
+++ app/backend/app/urls.py
@@ -16,18 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-]
-
-# users/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
     path('register/', views.register_user, name='register_user')
 ]
+
