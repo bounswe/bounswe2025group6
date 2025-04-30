467b2cd92c2ae8fe101cc925ab8153989752d2e4
celilozknn <celilozkan76@gmail.com>
2025-04-30 16:36:31 +0300

    add an dummy index page

--- app/backend/fithub/fithub/urls.py
+++ app/backend/fithub/fithub/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import index  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # include the api urls
     path('api/', include('api.urls')),
+    path('', index, name='index_page'),  # Map the root URL to the index page view
 ]
 
