ba1858f9edbbc8050d8fd6875c60486616a0772e
celilozknn <celilozkan76@gmail.com>
2025-04-30 18:31:35 +0300

    remove unnec print line

--- backend/fithub/fithub/settings.py
+++ backend/fithub/fithub/settings.py
@@ -19,7 +19,6 @@ pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(f"BASE_DIR: {BASE_DIR}")
 
 # Load environment variables from .env file
 load_dotenv(os.path.join(BASE_DIR, '.env'))
