0f5abdee505bfa5145bafe6cde42e158b03f5f88
ahmeterthacioglu <128293263+ahmeterthacioglu@users.noreply.github.com>
2025-05-01 17:12:02 +0300

    refactor(router): limit imports and routes to implemented pages only

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -16,6 +16,7 @@ function Layout() {
     </>
   );
 }
+
 function App() {
   return (
 
@@ -28,9 +29,9 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<ProfilePage />} />
     </Route>
+
     </Routes>
     </BrowserRouter>
-
   );
 }
 
