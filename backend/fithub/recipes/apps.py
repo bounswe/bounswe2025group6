608e1faf8050ef5ee99eb27dfc82769c07e57d03
celilozknn <celilozkan76@gmail.com>
2025-05-03 15:50:04 +0300

    add signals.py for like count update

--- backend/fithub/recipes/apps.py
+++ backend/fithub/recipes/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
+
+    # Import and register the signals when the app is ready
+    # Like, rate, comment count updates
+    def ready(self):
+        import recipes.signals  # This connects the signals in signals.py
