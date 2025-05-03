5e1965c15fd11fd95be9e37dabf8e72a2ac55fd0
celilozknn <celilozkan76@gmail.com>
2025-05-03 22:22:57 +0300

    add endpoint docs

--- backend/fithub/ingredients/urls.py
+++ backend/fithub/ingredients/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import IngredientViewSet
 
 router = DefaultRouter()
-router.register(r'ingredients', IngredientViewSet, basename='ingredient')
+router.register(r'', IngredientViewSet, basename='ingredient')
 
 urlpatterns = router.urls
