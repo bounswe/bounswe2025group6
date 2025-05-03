e9c57b646864aac7d235aa860c925841cf378e6a
celilozknn <celilozkan76@gmail.com>
2025-05-03 16:26:00 +0300

    add recipes and other related urls

--- backend/fithub/recipes/urls.py
+++ backend/fithub/recipes/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RecipeViewSet, RecipeLikeViewSet, IngredientViewSet
+
+# Initialize the DefaultRouter, which automatically generates routes for the ViewSets
+router = DefaultRouter()
+router.register(r'recipes', RecipeViewSet, basename='recipe')
+router.register(r'recipe_likes', RecipeLikeViewSet, basename='recipe_like')
+router.register(r'ingredients', IngredientViewSet, basename='ingredient')
 
 urlpatterns = [
-    # Without the 'api/' prefix
+    path('api/', include(router.urls)),
 ]
