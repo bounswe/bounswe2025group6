eb86df59bbca923b884645752fea3dd63b5e5858
celilozknn <celilozkan76@gmail.com>
2025-05-04 03:57:30 +0300

    remove ingredient app related stuff, they're written in a different branch before

--- backend/fithub/recipes/serializers.py
+++ backend/fithub/recipes/serializers.py
@@ -1,12 +1,8 @@
 # recipes/serializers.py
 from rest_framework import serializers
-from .models import Ingredient, Recipe, RecipeIngredient, RecipeLike
-from api.serializers import UserRegistrationSerializer  # Import UserSerializer from the appropriate module
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = '__all__'
+from api.serializers import UserRegistrationSerializer
+from ingredients.serializers import IngredientSerializer
+from .models import Recipe, RecipeIngredient, RecipeLike
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
     class Meta:
