0e46f9d6a1b43b63e9d5c71bfd3a1bd5ef8afb35
celilozknn <celilozkan76@gmail.com>
2025-05-04 15:06:47 +0300

    add endpoint to get all ingredient fields by name

--- backend/fithub/ingredients/views.py
+++ backend/fithub/ingredients/views.py
@@ -1,27 +1,11 @@
-from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Ingredient
-from .serializers import IngredientSerializer
-from rest_framework.pagination import PageNumberPagination
-from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework import mixins, viewsets  # Import mixins and viewsets
 from drf_yasg import openapi
-
-class IngredientPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-    def get_paginated_response(self, data):
-        page = self.page.number
-        page_size = self.page.paginator.per_page
-        return Response({
-            'page': page,
-            'page_size': page_size,
-            'count': self.page.paginator.count,
-            'total_pages': self.page.paginator.num_pages,
-            'results': data
-        })
+from drf_yasg.utils import swagger_auto_schema
+from .models import Ingredient  # Import the Ingredient model
+from .serializers import IngredientSerializer, IngredientPagination
 
 class IngredientViewSet(mixins.ListModelMixin,
                         mixins.RetrieveModelMixin,
@@ -30,6 +14,37 @@ class IngredientViewSet(mixins.ListModelMixin,
     serializer_class = IngredientSerializer
     pagination_class = IngredientPagination
 
+    # Function to get the Ingredient (all fields) by name
+    @swagger_auto_schema(
+        method='get',
+        manual_parameters=[
+            openapi.Parameter(
+                'name',
+                openapi.IN_QUERY,
+                description="Name of the ingredient",
+                type=openapi.TYPE_STRING,
+                required=True
+            )
+        ],
+        responses={200: openapi.Response(description='Ingredient object')},
+    )
+    @action(detail=False, methods=['get'], url_path='get-ingredient-by-name')
+    def get_ingredient_by_name(self, request):
+        name = request.query_params.get('name')
+        if not name:
+            return Response({'error': 'Query parameter "name" is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        name = ' '.join(name.strip().split())  # Normalize whitespace (remove extra spaces after, before, and between words)
+
+        try:
+            ingredient = Ingredient.objects.get(name=name)
+            serializer = self.serializer_class(ingredient)
+            return Response(serializer.data)
+        except Ingredient.DoesNotExist:
+            return Response({'error': 'Ingredient not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+    # Function to get the ID of an ingredient by its name
     @swagger_auto_schema(
         method='get',
         manual_parameters=[
@@ -48,7 +63,9 @@ class IngredientViewSet(mixins.ListModelMixin,
         name = request.query_params.get('name')
         if not name:
             return Response({'error': 'Query parameter "name" is required.'}, status=status.HTTP_400_BAD_REQUEST)
-        name = ' '.join(name.strip().split()) # Normalize whitespace (remove extra spaces after, before, and between words)
+
+        name = ' '.join(name.strip().split())  # Normalize whitespace (remove extra spaces after, before, and between words)
+
         try:
             ingredient = Ingredient.objects.get(name=name)
             return Response({'id': ingredient.id})
