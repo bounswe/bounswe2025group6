74866a8fb8021f8b1990a79348e9dad538cd8a5b
celilozknn <celilozkan76@gmail.com>
2025-05-08 02:00:42 +0300

    update comment view

--- backend/fithub/forum/urls.py
+++ backend/fithub/forum/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from forum.views import ForumPostViewSet, ForumPostCommentViewSet
 
 router = DefaultRouter()
-router.register(r'posts', ForumPostViewSet, basename='forumpost') # For forum post endpoints
-router.register(r'posts/(?P<post_id>\d+)/comments', ForumPostCommentViewSet, basename='forumpost-comments')  # Forum post comment endpoints
+router.register(r'posts', ForumPostViewSet, basename='forum-post') # For forum post endpoints
+router.register(r'posts/(?P<post_id>\d+)/comments', ForumPostCommentViewSet, basename='forum-post-comments')  # Forum post comment endpoints
 
 
 urlpatterns = [
--- backend/fithub/forum/views.py
+++ backend/fithub/forum/views.py
@@ -35,20 +35,65 @@ class ForumPostViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 @permission_classes([IsAuthenticatedOrReadOnly])
+permission_classes([IsAuthenticatedOrReadOnly])
 class ForumPostCommentViewSet(viewsets.ModelViewSet):
-    queryset = ForumPostComment.objects.all().order_by('-created_at')  # Order comments by creation date
     serializer_class = ForumPostCommentSerializer
+    pagination_class = StandardPagination  # Assuming you have a custom pagination class
 
-    # We want to filter the comments for a specific post
     def get_queryset(self):
+        """
+        Get the comments for a specific post.
+        """
         post = ForumPost.objects.get(id=self.kwargs['post_id'])
-        return post.comments.all()
+        return post.comments.filter(is_deleted=False)  # Only return non-deleted comments
 
     def perform_create(self, serializer):
+        """
+        Create a new comment associated with a post and user.
+        """
         post = ForumPost.objects.get(id=self.kwargs['post_id'])
         serializer.save(author=self.request.user, post=post)
 
     def create(self, request, *args, **kwargs):
-        # We can directly access the post_id in the URL, so we pass it to the serializer
+        """
+        Handle the creation of a comment.
+        """
         kwargs['post_id'] = self.kwargs['post_id']
         return super().create(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        """
+        List all comments for a specific post.
+        """
+        post = ForumPost.objects.get(id=self.kwargs['post_id'])
+        comments = post.comments.filter(is_deleted=False)
+        page = self.paginate_queryset(comments)
+        if page is not None:
+            serializer = ForumPostCommentSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = ForumPostCommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Get a single comment's details.
+        """
+        comment = self.get_object()
+        serializer = ForumPostCommentSerializer(comment)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Delete (soft delete) a comment.
+        Only the author can delete their comment.
+        """
+        comment = self.get_object()
+
+        if comment.author != request.user:
+            return Response({"detail": "You cannot delete a comment you did not create."}, status=status.HTTP_403_FORBIDDEN)
+
+        # Perform soft delete (mark as deleted)
+        comment.is_deleted = True
+        comment.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
