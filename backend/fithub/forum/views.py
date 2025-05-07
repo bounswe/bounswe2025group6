b5c9d4881dc78ba9a948f2fc10ccf2721a561ceb
celilozknn <celilozkan76@gmail.com>
2025-05-07 22:33:55 +0300

    add viewws for forumpost

--- backend/fithub/forum/views.py
+++ backend/fithub/forum/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# forum/views.py
+from rest_framework import viewsets
+from forum.models import ForumPost
+from utils.models import Tag
+from forum.serializers import ForumPostSerializer, TagSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class ForumPostViewSet(viewsets.ModelViewSet):
+    queryset = ForumPost.objects.all().order_by('-created_at')
+    serializer_class = ForumPostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
