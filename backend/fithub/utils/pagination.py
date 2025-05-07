8cd49ad64554369c458756ccf970c64a0fde1f89
celilozknn <celilozkan76@gmail.com>
2025-05-08 00:01:51 +0300

    update pagination response for util pagination

--- backend/fithub/utils/pagination.py
+++ backend/fithub/utils/pagination.py
@@ -1,7 +1,16 @@
 # utils/pagination.py
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class StandardPagination(PageNumberPagination):
     page_size = 10  # Default number of items per page
     page_size_query_param = 'page_size'  # Allow clients to specify page size
     max_page_size = 100  # Limit on the maximum number of items a client can request
+
+    def get_paginated_response(self, data):
+        return Response({
+            'page': self.page.number,
+            'page_size': self.get_page_size(self.request),
+            'total': self.page.paginator.count,
+            'results': data,
+        })
