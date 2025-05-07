43b82f39ec567a71b0ca6a37618b0a244a6761e5
celilozknn <celilozkan76@gmail.com>
2025-05-08 01:47:48 +0300

    add views for post comment

--- backend/fithub/forum/views.py
+++ backend/fithub/forum/views.py
@@ -1,8 +1,8 @@
 # forum/views.py
 from rest_framework import viewsets
 from utils.pagination import StandardPagination
-from forum.models import ForumPost
-from forum.serializers import ForumPostSerializer
+from forum.models import ForumPost, ForumPostComment
+from forum.serializers import ForumPostSerializer, ForumPostCommentSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
@@ -33,3 +33,22 @@ class ForumPostViewSet(viewsets.ModelViewSet):
         # Serialize and return the post
         serializer = self.get_serializer(post)
         return Response(serializer.data)
+
+@permission_classes([IsAuthenticatedOrReadOnly])
+class ForumPostCommentViewSet(viewsets.ModelViewSet):
+    queryset = ForumPostComment.objects.all().order_by('-created_at')  # Order comments by creation date
+    serializer_class = ForumPostCommentSerializer
+
+    # We want to filter the comments for a specific post
+    def get_queryset(self):
+        post = ForumPost.objects.get(id=self.kwargs['post_id'])
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = ForumPost.objects.get(id=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
+
+    def create(self, request, *args, **kwargs):
+        # We can directly access the post_id in the URL, so we pass it to the serializer
+        kwargs['post_id'] = self.kwargs['post_id']
+        return super().create(request, *args, **kwargs)
