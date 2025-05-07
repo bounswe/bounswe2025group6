6adf37821a8ca81894210bdcab43098a8c5c5d4c
celilozknn <celilozkan76@gmail.com>
2025-05-08 01:35:42 +0300

    add abstract comment class

--- backend/fithub/utils/models.py
+++ backend/fithub/utils/models.py
@@ -16,3 +16,39 @@ class PostModel(TimestampedModel):
 
     def __str__(self):
         return f"Post #{self.pk}, {self.title}"
+
+# Abstract base class for comments, will be used in forum and q/a models
+class CommentModel(TimestampedModel):
+    author = models.ForeignKey('api.RegisteredUser', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    parent_comment = models.ForeignKey(
+        'self',
+        related_name='replies',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    upvote_count = models.PositiveIntegerField(default=0)
+    downvote_count = models.PositiveIntegerField(default=0)
+    reported_count = models.PositiveIntegerField(default=0)
+
+    # This makes the model abstract and cannot be instantiated directly
+    # We will use this class to create comments for posts and use as subclass
+    # We don't want to create a table for this class
+    class Meta:
+        abstract = True
+
+    def upvote(self):
+        """Increment the upvote count"""
+        self.upvote_count += 1
+        self.save()
+
+    def downvote(self):
+        """Increment the downvote count"""
+        self.downvote_count += 1
+        self.save()
+
+    def report(self):
+        """Increment the report count for the comment"""
+        self.reported_count += 1
+        self.save()
