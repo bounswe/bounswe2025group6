8caab3789ce82ced25402ba1156a25e734e280b3
celilozknn <celilozkan76@gmail.com>
2025-05-12 00:48:09 +0300

    add some recipe images to fe, fe can consume these

---
+++ practice-app/frontend/src/assets/recipe_images/recipeImages.js
@@ -0,0 +1,41 @@
+import image1 from './image_1.jpg';
+import image2 from './image_2.jpg';
+import image3 from './image_3.jpg';
+import image4 from './image_4.jpg';
+import image5 from './image_5.jpg';
+import image6 from './image_6.jpg';
+import image7 from './image_7.jpg';
+import image8 from './image_8.jpg';
+import image9 from './image_9.jpg';
+import image10 from './image_10.jpg';
+import image11 from './image_11.jpg';
+import image12 from './image_12.jpg';
+import image13 from './image_13.jpg';
+import image14 from './image_14.jpg';
+import image15 from './image_15.jpg';
+import image16 from './image_16.jpg';
+import image17 from './image_17.jpg';
+import image18 from './image_18.jpg';
+import image19 from './image_19.jpg';
+
+export {
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+};
