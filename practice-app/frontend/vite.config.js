5f94d90680a0534401fb2283e8e7de1d4dfa6ebb
Cem Sarpkaya <cemsarpkaya@gmail.com>
2025-10-20 21:11:44 +0000

    https configuration added

--- practice-app/frontend/vite.config.js
+++ practice-app/frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import path from 'path';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'fullchain.pem')),
+    },
+
+    host: '0.0.0.0',
+    port: 443
+  },
 })
